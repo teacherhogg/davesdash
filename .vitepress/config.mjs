a92127c342ed24f60b79b48347a7fa6a2b4ee2e1
teacherhogg <teacherhogg@gmail.com>
2026-05-28 12:09:15 -0600

    Mravault Links

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -45,6 +45,7 @@ export default defineConfig({
         text: 'Topics',
         items: [
           { text: 'Photos', link: '/topics/photos' },
+          { text: 'Mira Vaults', link: '/topics/miravaults' },
           { text: 'Tailscale', link: '/topics/tailscale' },
           { text: 'Syncthing', link: '/topics/syncthing' },
           { text: 'Smart Home', link: '/topics/smarthome' },
---
+++ topics/miravaults.md
@@ -0,0 +1,11 @@
+---
+layout: doc
+title: Miravaults
+image: /images/item-icons/miraprompt.png
+---
+
+<script setup>
+import cards from '../data/tp-miravaults.json'
+</script>
+
+<CardGridEx :cards="cards" />
